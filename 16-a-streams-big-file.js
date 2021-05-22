dee74a74698739e4eec1cf160007649237bf423a
BasTion <therapyhangout@gmail.com>
2021-05-22 11:46:42 +0200

    big file energy

---
+++ 16-a-streams-big-file.js
@@ -0,0 +1,7 @@
+// creating a big file as prep for the streaming examples
+
+const { writeFileSync } = require('fs');
+
+for (let i = 0; i < 1000; i++) {
+	writeFileSync('./content/big.txt', `Hello World ${i}\n`, { flag: 'a' });
+}
